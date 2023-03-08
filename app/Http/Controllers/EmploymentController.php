c9bb7cb6dd78f32591b0aecc8fe7aa70569d87f0
Andre Neves <andre_neve@hotmail.com>
2023-03-08 15:28:08 -0300

    feat(api): creation of controllers

---
+++ app/Http/Controllers/EmploymentController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employment;
+use Illuminate\Http\Request;
+
+class EmploymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employment $employment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Employment $employment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employment $employment)
+    {
+        //
+    }
+}
